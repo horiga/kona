5c7c7799ffca62f19b4c3cb8ba4fa37fb0ff1820
Hiroyuki Horigami <horiga@horiga-mb.local>
2016-01-04 12:03:36 +0900

    Refactor

--- src/main/java/org/horiga/kona/spring/ServletRequestMetricFilter.java
+++ src/main/java/org/horiga/kona/spring/ServletRequestMetricFilter.java
@@ -4,36 +4,44 @@ package org.horiga.kona.spring;
 import com.netflix.servo.monitor.Counter;
 import com.netflix.servo.monitor.Monitors;
 import lombok.extern.slf4j.Slf4j;
-import org.jboss.netty.handler.codec.http.HttpResponseStatus;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.filter.OncePerRequestFilter;
 
 import javax.annotation.PostConstruct;
-import javax.servlet.AsyncContext;
-import javax.servlet.AsyncEvent;
-import javax.servlet.AsyncListener;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Objects;
-import java.util.UUID;
+import java.util.*;
 
 @Slf4j
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class ServletRequestMetricFilter extends OncePerRequestFilter {
 
 	private static Map<String, Counter> monitors;
 
+	private static Map<HttpStatus, Counter> errorMonitors;
+
 	@PostConstruct
 	public void init() {
 		monitors = new HashMap<>();
-		monitors.put("2xx", Monitors.newCounter("servlet-http-status-2xx"));
-		monitors.put("3xx", Monitors.newCounter("servlet-http-status-3xx"));
-		monitors.put("4xx", Monitors.newCounter("servlet-http-status-4xx"));
-		monitors.put("5xx", Monitors.newCounter("servlet-http-status-5xx"));
-		monitors.put("xxx", Monitors.newCounter("servlet-http-status-unknown"));
+		monitors.put("unknown", Monitors.newCounter("servlet-http-status-unknown"));
+		Arrays.asList(HttpStatus.Series.values())
+				.stream()
+				.forEach(s -> monitors.put(s.name().toLowerCase(), Monitors.newCounter("servlet-http-status-" + s.name().toLowerCase())));
+		errorMonitors = new HashMap<>();
+		Arrays.asList(HttpStatus.values())
+				.stream()
+				.filter(s -> containsErrorStatusMonitor(s))
+				.forEach(s -> errorMonitors.put(s, Monitors.newCounter("servlet-errors-" + s.value())));
+	}
+
+	@Override
+	public boolean shouldNotFilterAsyncDispatch() {
+		return false;
 	}
 
 	@Override
@@ -41,59 +49,41 @@ public class ServletRequestMetricFilter extends OncePerRequestFilter {
 									final HttpServletResponse servletResponse,
 									final FilterChain filterChain) throws ServletException, IOException {
 		try {
-			if (Objects.isNull(servletRequest.getAttribute("servlet-metric-session-id")))
-				servletRequest.setAttribute("servlet-metric-session-id", UUID.randomUUID().toString());
 			filterChain.doFilter(servletRequest, servletResponse);
 		} finally {
-			if( null == monitors) {
-				// Do not anything.
-				return;
-			}
-			if (isAsyncStarted(servletRequest)) {
-				AsyncContext ctx = servletRequest.getAsyncContext();
-				if(!Objects.isNull(ctx)) {
-					ctx.addListener(new AsyncListener() {
-						@Override
-						public void onComplete(AsyncEvent event) throws IOException {
-							log.debug("async-servlet: onComplete");
-							serve(servletRequest, servletResponse);
-						}
-
-						@Override
-						public void onTimeout(AsyncEvent event) throws IOException {
-							log.debug("async-servlet: onTimeout");
-							serve(servletRequest, servletResponse);
-						}
-
-						@Override
-						public void onError(AsyncEvent event) throws IOException {
-							log.debug("async-servlet: onError");
-							serve(servletRequest, servletResponse);
-						}
-
-						@Override
-						public void onStartAsync(AsyncEvent event) throws IOException {
-							log.debug("async-servlet: onStartAsync");
-						}
-					});
-				} else {
-					//
-				}
-			} else {
-				serve(servletRequest, servletResponse);
+			if (!isAsyncStarted(servletRequest)) {
+				markMetrics(servletRequest, servletResponse);
 			}
 		}
 	}
 
-	protected void serve(HttpServletRequest servletRequest, HttpServletResponse servletResponse) {
-		int sc = servletResponse.getStatus();
-		if ( sc >= 200) {
-			int n = sc / 100;
-			String key = (n <= 5) ? n + "xx" : "xxx";
-			monitors.get(key).increment();
-		} else {
-			// monitors.get("xxx").increment();
+	protected void markMetrics(final HttpServletRequest servletRequest,
+                               final HttpServletResponse servletResponse) {
+		String seriesName = "unknown";
+		Optional<HttpStatus> status = status(servletResponse);
+        if (status.isPresent()) {
+            if ( containsErrorStatusMonitor(status.get())) {
+                errorMonitors.get(status.get()).increment();
+            }
+            HttpStatus.Series series = status.get().series();
+            if (!Objects.isNull(series)) {
+                seriesName = series.name();
+            }
+        }
+		monitors.get(seriesName.toLowerCase()).increment();
+	}
+
+	private Optional<HttpStatus> status(HttpServletResponse response) {
+		try {
+			return Optional.of(HttpStatus.valueOf(response.getStatus()));
+		}catch (Exception e) {
+			// IllegalArgumentException
+			return Optional.empty();
 		}
 	}
+
+    private boolean containsErrorStatusMonitor(HttpStatus s) {
+        return s.is4xxClientError() || s.is5xxServerError();
+    }
 
 }
